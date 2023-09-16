8e1a8cfc9f382b563c496190f1003785f342b9ae
sabrinara <you@example.com>
2023-09-16 18:20:07 +0600

    course credit functionality done

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,24 +1,38 @@
-export default function Cart(){
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Cart = ({ selectedCourses, remaining, totalHour, totalPrice }) => {
+    console.log(selectedCourses);
     return (
         <div className="card bg-base-100 shadow-2xl w-[220px]">
             <div className="text-blue-700 font-bold ">
-                <h5>Credit Hour Remaining: </h5>
+                <h5>Credit Hour Remaining:{remaining} hr</h5>
             </div>
             <div className="w-full h-[1px] bg-gray-200 my-2"></div>
             <div className="">
                 <h2 className="font-bold text-xl text-left">Course Name</h2>
-                <ol type="1" className="text-gray-400 text-left">
-                    <li>ghgchafsj</li>
-                </ol>
+                <div className="text-gray-400 text-left">
+                    <ol type='1'>
+                        {selectedCourses.map((course) => (
+                            <li key={course.id}>{course.title}</li>
+                        ))}
+                    </ol>
+                </div>
+
             </div>
             <div className="w-full h-[1px] bg-gray-200 my-2"></div>
-            <div className="text-gray-500 font-semibold text-lg text-left">
-                <h1>Total Credit Hour:</h1>
+            <div className="text-gray-500 font-semibold text-md text-left">
+                <h3>Total Credit Hour: {totalHour} hr</h3>
             </div>
             <div className="w-full h-[1px] bg-gray-200 my-2"></div>
-            <div className="text-gray-700 font-semibold text-lg text-left">
-                <h1>Total Price:USD</h1>
+            <div className="text-gray-700 font-semibold text-md text-left">
+                <h3>Total Price: {totalPrice} USD</h3>
             </div>
         </div>
-    )
-}
+    );
+
+};
+
+export default Cart;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,10 @@ import Cart from "../Cart/cart";
 
 export default function Home() {
     const [allCourses, setAllCourses] = useState([]);
+    const [selectedCourses, setSelectedCourses] = useState([]);
+    const [remaining, setRemaining] = useState(0);
+    const [totalHour, setTotalHour] = useState(0);
+    const [totalPrice, setTotalPrice] = useState(0);
 
 
     useEffect(() => {
@@ -10,6 +14,37 @@ export default function Home() {
             .then((res) => res.json())
             .then((data) => setAllCourses(data));
     }, []);
+
+    const handleSelectCourse = (course) => {
+        const isExist = selectedCourses.find((item) => item.id == course.id);
+        let hour = course.credit;
+        let price = course.price;
+
+        if (isExist) {
+          return alert("Already booked");
+        }
+        else {
+          selectedCourses.forEach(item => {
+            hour += item.credit;
+            price +=item.price;
+          });
+    
+          const remaining = 20 - hour;
+    
+          if (hour > 20) {
+            return alert("No more credit");
+          }
+          else {
+            
+            setRemaining(remaining);
+            setTotalHour(hour);
+            setTotalPrice(price);
+            setSelectedCourses([...selectedCourses, course]);
+          }
+    
+        }
+    
+      };
 
     return (
         <div className="max-w-7xl mx-auto flex gap-3 mt-6 justify-between px-2 pb-10">
@@ -33,7 +68,7 @@ export default function Home() {
                             <p className="ml-4"><i className="fa-solid fa-book-open"></i> Credit:{course.credit}hr</p>
                         </div>
                         <div>
-                            <button className="rounded-lg text-white text-center px-24 mt-1 bg-blue-700  btn">Select</button>
+                            <button className="rounded-lg text-white text-center px-24 mt-1 bg-blue-700  btn" onClick={() => handleSelectCourse(course)} >Select</button>
                         </div>
                     </div>            
                 )
@@ -41,7 +76,11 @@ export default function Home() {
                 }
             </div>
             <div className="cart">
-                <Cart></Cart>
+                <Cart selectedCourses = {selectedCourses}
+                remaining = {remaining}
+                totalHour = {totalHour}
+                totalPrice = {totalPrice}
+                ></Cart>
             </div>
         </div>
     )
