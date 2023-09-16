b03a4ede3b57c6471c59fa590a27467c7daff266
sabrinara <you@example.com>
2023-09-16 17:31:02 +0600

    raw card json added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,24 @@
 export default function Cart(){
     return (
-        <div>
-            <h1>this cart</h1>
+        <div className="card bg-base-100 shadow-2xl w-[220px]">
+            <div className="text-blue-700 font-bold ">
+                <h5>Credit Hour Remaining: </h5>
+            </div>
+            <div className="w-full h-[1px] bg-gray-200 my-2"></div>
+            <div className="">
+                <h2 className="font-bold text-xl text-left">Course Name</h2>
+                <ol type="1" className="text-gray-400 text-left">
+                    <li>ghgchafsj</li>
+                </ol>
+            </div>
+            <div className="w-full h-[1px] bg-gray-200 my-2"></div>
+            <div className="text-gray-500 font-semibold text-lg text-left">
+                <h1>Total Credit Hour:</h1>
+            </div>
+            <div className="w-full h-[1px] bg-gray-200 my-2"></div>
+            <div className="text-gray-700 font-semibold text-lg text-left">
+                <h1>Total Price:USD</h1>
+            </div>
         </div>
     )
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,33 +1,48 @@
+import { useEffect, useState } from "react";
 import Cart from "../Cart/cart";
 
 export default function Home() {
+    const [allCourses, setAllCourses] = useState([]);
+
+
+    useEffect(() => {
+        fetch("./data.json")
+            .then((res) => res.json())
+            .then((data) => setAllCourses(data));
+    }, []);
+
     return (
-        <div className="max-w-7xl mx-auto flex gap-5 justify-between px-2 pb-10">
-            <div className="grid md:grid-cols-3">
-                 <div className='flex flex-wrap  product-item product card w-[320px] bg-base-100 shadow-2xl '>
-                <div className="text-left mb-2 ">
-                    <img src="https://i.ibb.co/Drj16Ck/Rectangle-2-2.png" alt="" />
-                </div>
-                <div className="text-left">
-                    <h3 className="font-bold">Introduction to C Programming</h3>
-                    <p>
-                        <small>
-                        C is a general-purpose programming language created by Dennis Ritchie at the Bell Laboratories in 1972. It is a very popular language, despite being old.
-                        </small>
-                    </p>
-                </div>
-                <div className="flex flex-wrap justify-content: space-evenly gap-6 mt-2 mb-2">
-                    <p ><i class="fa-solid fa-dollar-sign"></i> Price : 15000</p>
-                    <p className="ml-4"><i class="fa-solid fa-book-open"></i> Credit: 1hr</p>
-                </div>
-                <div>
-                <button className="rounded-lg text-white text-center px-16   bg-blue-700  btn">Select</button>
-                </div>
+        <div className="max-w-7xl mx-auto flex gap-3 mt-6 justify-between px-2 pb-10">
+            <div className="grid md:grid-cols-3 gap-5">
+                {allCourses.map((course) => (
+
+                    <div key={course.id} className='flex flex-wrap card w-[310px] bg-base-100 shadow-2xl '>
+                        <div className=" mb-2 ">
+                            <img src={course.image} alt="" />
+                        </div>
+                        <div className="text-left">
+                            <h3 className="font-bold mt-2 text-md">{course.title}</h3>
+                            <p className="w-full h-20 overflow-hidden">
+                                <small>
+                                   {course.description}
+                                </small>
+                            </p>
+                        </div>
+                        <div className="flex flex-wrap justify-content: space-evenly gap-1 mt-1 mb-2">
+                            <p ><i className="fa-solid fa-dollar-sign"></i> Price : {course.price}$</p>
+                            <p className="ml-4"><i className="fa-solid fa-book-open"></i> Credit:{course.credit}hr</p>
+                        </div>
+                        <div>
+                            <button className="rounded-lg text-white text-center px-24 mt-1 bg-blue-700  btn">Select</button>
+                        </div>
+                    </div>            
+                )
+                )
+                }
             </div>
+            <div className="cart">
+                <Cart></Cart>
             </div>
-                <div className="cart">
-                    <Cart></Cart>
-                </div> 
         </div>
     )
 }
